5ef29f6bcdc1a23f9e111d2cc3c35328c0dc8622
readycatholic <jamescoogan@gmail.com>
2026-09-20 18:42:08 -0400

    St. Jude ad loader — loads 3 rotating creatives

--- parish/js/stjude-ads.js
+++ parish/js/stjude-ads.js
@@ -3,16 +3,24 @@
  * Link: http://fundraising.stjude.org/goto/ReadyCatholic
  */
 (function () {
-  var ads = [];
-  // populated below
   var img = document.getElementById("stjude-ad-img");
   if (!img) return;
-  // Load creatives from separate files to keep this loader small
+  var base = "";
+  try {
+    var scripts = document.getElementsByTagName("script");
+    for (var i = 0; i < scripts.length; i++) {
+      if (scripts[i].src && scripts[i].src.indexOf("stjude-ads.js") !== -1) {
+        base = scripts[i].src.replace(/stjude-ads\.js.*$/, "");
+        break;
+      }
+    }
+  } catch (e) {}
+  if (!base) base = "../../js/";
   var files = ["stjude-ad-1.js", "stjude-ad-2.js", "stjude-ad-3.js"];
   var loaded = 0;
-  files.forEach(function (f, idx) {
+  files.forEach(function (f) {
     var s = document.createElement("script");
-    s.src = (document.currentScript && document.currentScript.src.replace(/stjude-ads\.js.*/, "") || "../../js/") + f;
+    s.src = base + f;
     s.onload = function () {
       loaded++;
       if (loaded === files.length && window.__STJUDE_ADS && window.__STJUDE_ADS.length) {
