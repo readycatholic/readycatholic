ba37134ca923311e9552c37396c28f9ccd574cc4
readycatholic <readycatholic@users.noreply.github.com>
2026-09-24 17:46:05 +0000

    deploy: e0c5c9eff9ee86708fd982d5c44a397ddffb9bd3

--- scripts/generate_parish_pages.py
+++ scripts/generate_parish_pages.py
@@ -92,6 +92,7 @@ def load_parishes():
         "parishes-belleville-b.json",
         "parishes-belleville-c.json",
         "parishes-belleville-d.json",
+        "parishes-belleville-e.json",
     ]
     seen = set()
     out = []
